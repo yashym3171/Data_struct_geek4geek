6281dec7cdbc9c794f41578ab7da172395e3dff5
prathamiitp <75429858+prathamiitp@users.noreply.github.com>
2021-02-24 19:49:47 +0530

    Update deleteNode.cpp

--- deleteNode.cpp
+++ deleteNode.cpp
@@ -13,16 +13,20 @@ struct Node
 
 /*You are required to complete below method*/
 Node* deleteNode(Node *head,int x){
-    struct Node* current=new Node(0);
+    struct Node* current=NULL;
+    struct Node* current1=NULL;
     current=head;
+    current1=head->next;
     
     if(x==1)
         return head->next;
         
     for(int i=1; i<x-1; i++){
         current=current->next;
+        current1=current1->next;
     }
-    current->next = current->next->next;
+    current->next = current->next->next; //or current->next=current1->next;
+    delete current1;
     
     return head;
 }
